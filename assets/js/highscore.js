66ef2acd6721328a83d5b0fc0d8e40fa49f3d515
Eldar <elogon@gmail.com>
2020-10-25 13:17:29 -0400

    implement highscore record based on time left and minor css improvements

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,14 +1,21 @@
 var users = JSON.parse(localStorage.getItem('users')); // get user from localstorage
 var userListEl = document.getElementById('user-list'); // user list Element
 
-console.log(users);
-userListEl.innerHTML = ''; // Clear any previous list
-// Display list of users with highscores
-for (var i  = 0; i < users.length; i++) {
-    var liElement = document.createElement('li');
-    liElement.textContent = users[i].name + ' - ' + users[i].highscore;
-    userListEl.appendChild(liElement);
-}
+userListEl.innerHTML = ''; // Clear any previous list from page
+
+// Display list of users with highscores only if there are saved users
+if (users) {
+    // Sort users from high to low score
+    users.sort(function(a,b) {
+        return b.highscore - a.highscore;
+    });
+
+    for (var i  = 0; i < users.length; i++) {
+        var liElement = document.createElement('li');
+        liElement.textContent = users[i].name + ' - ' + users[i].highscore;
+        userListEl.appendChild(liElement);
+    }
+} else userListEl.innerHTML = 'No records';
 
 function clearHighscore() {
     localStorage.removeItem('users');
